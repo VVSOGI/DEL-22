c179915291c090ef6165601bf61fa8f86123f479
benny <vvsogi@gmail.com>
2024-09-27 17:28:10 +0900

    Remove Google Analytics and unnecessary code

--- src/background.js
+++ src/background.js
@@ -14,9 +14,7 @@
 
 chrome.action.onClicked.addListener((tab) => {
   chrome.storage.sync.get({ optOutAnalytics: false }, (results) => {
-    const files = results.optOutAnalytics
-      ? ["script.js"]
-      : ["script.js", "ga.js"];
+    const files = ["script.js"];
     chrome.scripting.executeScript({
       target: { tabId: tab.id, allFrames: true },
       world: "MAIN",
